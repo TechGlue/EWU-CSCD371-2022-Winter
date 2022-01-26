35e37d88610c3944ddfff304777cbfa962109644
Luis Garcia <lgarcia29@eagles.ewu.edu>
2022-01-25 18:50:44 -0800

    BaseLoggerMixins tests done

--- Logger.Tests/BaseLoggerMixinsTests.cs
+++ Logger.Tests/BaseLoggerMixinsTests.cs
@@ -32,19 +32,74 @@
         }
 
         [TestMethod]
+        public void Warning_WithData_LogsMessage()
+        {
+            // Arrange
+            var logger = new TestLogger();
+            
+            // Act
+            BaseLoggerMixins.Warning(LogCategory.Warning, logger);
+
+            // Assert
+            Assert.AreEqual(1, logger.LoggedMessages.Count);
+            Assert.AreEqual(LogLevel.Warning, logger.LoggedMessages[0].LogLevel);
+            Assert.AreEqual("Message 41: Warning", logger.LoggedMessages[0].Message);
+        }
+       
+        [TestMethod]
         public void Error_WithData_LogsMessage()
         {
             // Arrange
             var logger = new TestLogger();
             
             // Act
-            BaseLoggerMixins.Error("Message 42", logger);
+            BaseLoggerMixins.Error(LogCategory.Error, logger);
 
             // Assert
             Assert.AreEqual(1, logger.LoggedMessages.Count);
             Assert.AreEqual(LogLevel.Error, logger.LoggedMessages[0].LogLevel);
-            Assert.AreEqual("Message 42", logger.LoggedMessages[0].Message);
+            Assert.AreEqual("Message 42: Error", logger.LoggedMessages[0].Message);
         }
+        
+        [TestMethod]
+        public void Information_WithData_LogsMessage()
+        {
+            // Arrange
+            var logger = new TestLogger();
+            
+            // Act
+            BaseLoggerMixins.Information(LogCategory.Information, logger);
+
+            // Assert
+            Assert.AreEqual(1, logger.LoggedMessages.Count);
+            Assert.AreEqual(LogLevel.Information, logger.LoggedMessages[0].LogLevel);
+            Assert.AreEqual("Message 40: Information", logger.LoggedMessages[0].Message);
+        }
+        
+        [TestMethod]
+        public void Debug_WithData_LogsMessage()
+        {
+            // Arrange
+            var logger = new TestLogger();
+            
+            // Act
+            BaseLoggerMixins.Debug(LogCategory.Debug, logger);
+
+            // Assert
+            Assert.AreEqual(1, logger.LoggedMessages.Count);
+            Assert.AreEqual(LogLevel.Debug, logger.LoggedMessages[0].LogLevel);
+            Assert.AreEqual("Message 39: Debug", logger.LoggedMessages[0].Message);
+        }
+    }
+
+    //Since for unit tests it does not like literals I created a class here that is dedicated just for containing error codes. 
+    //Inspiration: https://stackoverflow.com/questions/630803/associating-enums-with-strings-in-c-sharp
+    public static class LogCategory
+    {
+        public static string Error { get { return new string("Message 42: Error"); } }
+        public static string Warning { get { return new string("Message 41: Warning"); } }
+        public static string Information { get { return new string("Message 40: Information"); } }
+        public static string Debug { get { return new string("Message 39: Debug"); } }
     }
 
     public class TestLogger : BaseLogger
