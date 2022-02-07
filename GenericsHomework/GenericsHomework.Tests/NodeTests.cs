07c51d9c740889a0cbcfd9c4f0d9c7e395bb5d8d
Luis Garcia <lgarcia29@eagles.ewu.edu>
2022-02-06 16:02:58 -0800

    Contructor tests started.

--- GenericsHomework/GenericsHomework.Tests/NodeTests.cs
+++ GenericsHomework/GenericsHomework.Tests/NodeTests.cs
@@ -4,8 +4,33 @@ namespace GenericsHomework.Tests;
 public class NodeTests
 {
     [TestMethod]
-    public void TestMethod1()
+    public void Constructor_ValidValue_NodeIsNotNull()
     {
-        //hello
+        //Arrange
+        Node<int> node = new(42);
+        //Assert
+        Assert.IsNotNull(node);
+    }
+    
+    //maybe find a better name that is shorter 
+    [TestMethod]
+    public void Constructor_ValidValue_NodePointsToItselfOnCreation()
+    {
+        //Arrange
+        Node<int> node = new(42);
+        //Assert
+        Assert.AreEqual(node, node.Next);
+    }
+    
+    [TestMethod]
+    [DataRow(42)]
+    [DataRow(22)]
+    [DataRow(23)]
+    public void Constructor_ValidValue_NodeSucessfullyStoresValue(int value)
+    {
+        //Arrange
+        Node<int> node = new(value);
+        //Assert
+        Assert.AreEqual(node.Value, value);
     }
 }
